ecdfa501514ef734c6dfef655ea8bbc456b1b9d7
Mehakpree <prince56studio@gmail.com>
2026-08-03 23:23:18 +0530

    created the first react project

--- testing/index.js
+++
@@ -1,7 +0,0 @@
-let rvalue =0;
-while (rvalue !== 7) {
-    rvalue = Math.floor(Math.random()*10);
-    let div = document.createElement('div');
-    div.textContent = rvalue;
-    document.body.append(div)
-}
