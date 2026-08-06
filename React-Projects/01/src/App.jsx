431d7c14057d1152b921845286842c3f690ba4ba
Mehakpree <prince56studio@gmail.com>
2026-08-06 16:00:31 +0530

    tailwind css added

--- React-Projects/01/src/App.jsx
+++ React-Projects/01/src/App.jsx
@@ -1,18 +1,9 @@
-import Card from "./Components/Card";
+import React from 'react'
 
 const App = () => {
-  let arr = [
-    { name: "1", img: "https://i.pinimg.com/1200x/50/6d/b3/506db390a2eeb6f0211a25bea07b7a7d.jpg" },
-    { name: "2", img: "url" },
-    { name: "3", img: "url" },
-  ];
   return (
-    <div className="parent">
-      {arr.map((el,i) =>{
-        return <Card key={i} name = {el.name} img = {el.img} />
-      })}
-    </div>
-  );
-};
+    <div className='bg-red-500'>sApp</div>
+  )
+}
 
-export default App;
+export default App
